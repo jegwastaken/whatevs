00387fe99ddf624dc324ec43ed6418547ab7a721
John Romeral <devbazooka@gmail.com>
2024-03-19 12:21:46 -0400

    removed line breaks

--- 001-bard-highlighter.js
+++ 001-bard-highlighter.js
@@ -2,5 +2,5 @@ window.onload = function() {
     const table = document.querySelector("table");
     const regex = /(&lt;CO-CREATION-SELECTED-TEXT&gt;)([\s\S]*?)(&lt;\/CO-CREATION-SELECTED-TEXT&gt;)/g;
     table.innerHTML = table.innerHTML.replace(regex, 
-        '<span style="color:blue;font-weight:bold;">$1</span><br><span style="background-color:#e0f7fa;">$2</span><br><span style="color:blue;font-weight:bold;">$3</span>');
+        '<span style="color:blue;font-weight:bold;">$1</span><span style="background-color:#e0f7fa;">$2</span><span style="color:blue;font-weight:bold;">$3</span>');
 };
