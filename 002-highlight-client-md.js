833c59cf082a253d6744268745234d8254304021
John Romeral <devbazooka@gmail.com>
2024-05-18 15:07:32 -0400

    Update 002-highlight-client-md.js

--- 002-highlight-client-md.js
+++ 002-highlight-client-md.js
@@ -3,12 +3,14 @@ window.onload = function () {
 
   elements.forEach((element) => {
     let htmlString = element.innerHTML;
-    const regex = /&lt;MODELGENERATED&gt;([\s\S]*?)&lt;\/MODELGENERATED&gt;/g;
 
-    htmlString = htmlString.replace(
-      regex,
-      '<span style="background:rgba(255,255,0,.1);display:inline-block;">$1</span>'
-    );
+    const regex1 = /&lt;MODELGENERATED&gt;([\s\S]*?)&lt;\/MODELGENERATED&gt;/g;
+    const regex2 = /<span class="hljs-tag">&lt;<span class="hljs-name">MODELGENERATED<\/span>&gt;<\/span>([\s\S]*?)<span class="hljs-tag">&lt;\/<span class="hljs-name">MODELGENERATED<\/span>&gt;<\/span>/g;
+    const regex3 = /<span class="hljs-operator">&lt;<\/span>MODELGENERATED<span class="hljs-operator">&gt;<\/span>([\s\S]*?)<span class="hljs-operator">&lt;<\/span><span class="hljs-operator">\/<\/span>MODELGENERATED<span class="hljs-operator">&gt;<\/span>/g;
+
+    htmlString = htmlString.replace(regex1, '<span style="background:rgba(255,255,0,.1);display:inline-block;">$1</span>');
+    htmlString = htmlString.replace(regex2, '<span style="background:rgba(255,255,0,.1);display:inline-block;">$1</span>');
+    htmlString = htmlString.replace(regex3, '<span style="background:rgba(255,255,0,.1);display:inline-block;">$1</span>');
 
     element.innerHTML = htmlString;
   });
